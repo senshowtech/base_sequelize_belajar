2cb7676534c42031111094369eb8f52f953812ed
sena <hargiantofebrisena@gmail.com>
2022-04-18 13:33:34 +0700

    initial

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,14 +1,16 @@
+const { user } = require("../../models");
+
 exports.addUser = async (req, res) => {
   try {
     let { name, email, password, status } = req.body;
-    let create_product = await product.create({
+    let create_user = await user.create({
       name: name,
       email: email,
       password: password,
       status: status,
     });
     return res.status(201).json({
-      create_product,
+      create_user,
     });
   } catch (error) {
     console.log(error);
